4bc4be81e3085dd34f5a58b7b4b27e35650d57bf
aydinmfatih <aydinmfatih@gmail.com>
2024-04-23 17:27:45 +0300

    Add project files.

---
+++ SignalR.Web/Hubs/Examplehub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Web.Hubs
+{
+    public class Examplehub:Hub
+    {
+        public async Task BroadCastMessageToAllClient(string message)
+        {
+            await Clients.All.SendAsync("RecieveMessageForAllClient",message);
+        }
+    }
+}
---
+++ SignalR.Web/wwwroot/js/signalr-client.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+
+    const connection = new signalR.HubConnectionBuilder().withUrl("/examplehub").configureLogging(signalR.LogLevel.Information).build();
+
+
+    function start() {
+        connection.start().then(() => console.log("hub ile Bağlantı Kuruldu"));
+    }
+
+    try {
+        start()
+    }
+    catch (e) {
+        setTimeout(() => start(), 2000)
+    }
+
+})
